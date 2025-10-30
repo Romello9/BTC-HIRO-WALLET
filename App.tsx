38568688c6c7aa64de090490b8105ac65a36f126
Romello9 <romellini.andrea@hotmail.it>
2025-10-30 23:55:30 +0100

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -4,6 +4,14 @@ import WalletInterface from './components/WalletInterface';
 
 export default function App() {
   return (
+    return (
+  <div>
+    <h1>Il mio Wallet BTC/STX</h1>
+    <button onClick={connectWallet}>🔗 Connetti il tuo Hiro Wallet</button>
+    <p id="walletAddress">Nessun wallet collegato</p>
+  </div>
+);
+
     <WalletInterface />
   );
 }
